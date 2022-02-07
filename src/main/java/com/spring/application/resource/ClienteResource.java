1f0236dd7f0f6988a9bb7b193bbf179da92d4a68
JoseapNeto <netimsccp@gmail.com>
2022-02-06 21:35:23 -0300

    Produção

--- src/main/java/com/spring/application/resource/ClienteResource.java
+++ src/main/java/com/spring/application/resource/ClienteResource.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/clientes")
-@CrossOrigin("http://localhost:4200")
+//@CrossOrigin("http://localhost:4200")
 public class ClienteResource {
 
 
--- src/main/java/com/spring/application/resource/ServicoResource.java
+++ src/main/java/com/spring/application/resource/ServicoResource.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/address")
-@CrossOrigin("http://localhost:4200")
+//@CrossOrigin("http://localhost:4200")
 public class ServicoResource {
 
     @Autowired
